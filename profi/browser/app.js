23980e9837ddb4239ef0cd9a6015aa4351722c67
Anton Kartashov <antkartashov@gmail.com>
2024-03-05 14:45:55 +0500

    scroll

--- profi/browser/app.js
+++ profi/browser/app.js
@@ -1,9 +1,32 @@
 let bg = new BackgroundLayer ({
   backgroundColor: '#e5e5e5',
-  backgroundColor: '#111',
+  backgroundColor: '#3C3F49',
 });
 
-let layerA = new Layer({
-  point: Align.center(),
-  backgroundColor: 'tomato'
+let screen = new Layer({
+  backgroundColor: '#3C3F49',
+  size: Screen.size,
+  clip: true
 });
+
+let white = new ScrollComponent({
+  parent: screen,
+  width: Screen.width,
+  height: Screen.height - 228 * 2,
+  y: 228 * 2,
+  backgroundColor: 'white',
+  borderRadius: 32 * 2,
+  scrollHorizontal: false,
+});
+
+for (let i = 0; i < 20; i++) {
+  let item = new Layer({
+    parent: white.content,
+    width: Screen.width - 32 * 2,
+    x: 16 * 2,
+    y: 32 + i * 220,
+    opacity: 20,
+    borderRadius: 40,
+    opacity: .5
+  });
+}
