8a7fd9f99d1207e4b62798d1388960661812c47e
Anton Kartashov <antkartashov@gmail.com>
2024-03-05 14:33:05 +0500

    browser

---
+++ profi/browser/app.js
@@ -0,0 +1,9 @@
+let bg = new BackgroundLayer ({
+  backgroundColor: '#e5e5e5',
+  backgroundColor: '#111',
+});
+
+let layerA = new Layer({
+  point: Align.center(),
+  backgroundColor: 'tomato'
+});
